63692c986f3a0bcb62bd3cfc8da12e4a2390bf54
mjso <mjso@lumanlab.com>
2022-03-23 17:16:26 +0900

    test

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -24,7 +24,7 @@ const HomeScreen = () => {
     data: files,
   } = useQuery('fetchFiles', fetchFileList);
 
-  const a = '';
+  const a = 'aaa';
   // console.log("?? ", files);
 
   const dispatch = useAppDispatch();
